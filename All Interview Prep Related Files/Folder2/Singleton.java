73891f0dd0fadb668b33ccff19017bfa6cc7d23e
sandeepaswar1093 <sandeep1.aswar@gmail.com>
2022-09-16 10:59:29 +0530

    Interview Preparation

---
+++ All Interview Prep Related Files/Folder2/Singleton.java
@@ -0,0 +1,27 @@
+class Test
+{
+Prvate static Test t=null;
+
+Private Test()
+{}
+
+public static Test gettest()
+{
+if (t==null)
+{
+t=new Test()
+}
+class Client 
+{
+
+Public static void main(String[] args)
+{
+System.out.println(Test.getTets().hashcode());
+
+
+System.out.println(Test.getTets().hashcode());
+}
+}
+
+}
+}
